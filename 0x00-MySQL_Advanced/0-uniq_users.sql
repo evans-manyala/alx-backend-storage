ff2e4b41f6eec274945c79e96b50c45f046855bd
Evans Manyala <evans.manyala@gmail.com>
2024-07-11 23:46:58 +0300

    Adding error fix in code logic

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- Task: Create a table 'users' with specified attributes if it does not already exist.
-
 -- Creating the 'users' table with id, email, and name columns
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)
-);
+)
