e0786ac3c6acb0a7052b14bb082b7f947e206ee5
Evans Manyala <evans.manyala@gmail.com>
2024-07-12 11:07:52 +0300

    Adding fix to code logic

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,10 @@
 -- Task: Create a table 'users' with specified attributes if it does not already exist.
 -- Creating the 'users' table with id, email, name, and country columns
+
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country VARCHAR(255) NOT NULL DEFAULT 'US',
+        CONSTRAINT check_country CHECK (country IN ('US', 'CO', 'TN'))
 );
