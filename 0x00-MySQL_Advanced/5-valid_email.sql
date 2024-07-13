d086bdaedb70f9a820157e7bd93d32e6b77ea193
Evans Manyala <evans.manyala@gmail.com>
2024-07-13 18:19:37 +0300

    Error fix attempt 7

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,10 +2,10 @@
 -- the attribute valid_email only when the email
 -- has been changed
 
-CREATE TRIGGER reset_valid_email BEFORE 
-UPDATE 
-  ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN 
-SET 
-  NEW.valid_email = 0;
-END IF;
+CREATE OR REPLACE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+WHEN (NEW.email <> OLD.email)
+BEGIN
+    NEW.valid_email = false;
 END;
