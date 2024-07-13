45a8bcffca2a649f1b71e2658770f9e39ea9ab25
Evans Manyala <evans.manyala@gmail.com>
2024-07-13 12:03:18 +0300

    Adding script logi fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,12 @@
 -- Column names must be: band_name and lifespan (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
-
-SELECT band_name AS name, (2022 - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT 
+  `band_name`, 
+  IFNULL(split, 2022) - formed AS `lifespan` 
+FROM 
+  metal_bands 
+WHERE 
+  FIND_IN_SET("Glam Rock", style) 
+ORDER BY 
+  `lifespan` DESC;
