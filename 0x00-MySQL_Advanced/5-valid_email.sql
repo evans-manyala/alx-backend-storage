5e4bf03264c80e11dff3b266e5ac6413912e9566
Evans Manyala <evans.manyala@gmail.com>
2024-07-13 15:23:26 +0300

    Fixing errors in code logic

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,8 +4,11 @@
 -- for user email validation - distribute the logic
 -- to the database itself!
 
-CREATE TRIGGER reset_valid_email BEFORE 
-UPDATE 
-  ON users FOR EACH ROW BEGIN IF NEW.email != OLD.email THEN 
-SET 
-  NEW.valid_email = 0;
+CREATE TRIGGER reset_valid_email 
+BEFORE UPDATE ON users 
+FOR EACH ROW 
+BEGIN 
+        IF NEW.email <> OLD.email THEN 
+                SET NEW.valid_email = 0;
+        END IF;
+END;
