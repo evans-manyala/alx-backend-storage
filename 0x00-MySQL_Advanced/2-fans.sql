115f7ab6b930f45f2a092b335c179c5ba902469e
Evans Manyala <evans.manyala@gmail.com>
2024-07-13 14:28:16 +0300

    Errof fix attempt 6

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,19 +1,6 @@
 -- Task: Rank country origins of bands, ordered by the number of (non-unique) fans.
 
-DROP 
-  TABLE IF EXISTS temp_country_fans;
-CREATE TEMPORARY TABLE temp_country_fans AS 
-SELECT 
-  origin, 
-  SUM(nb_fans) AS nb_fans 
-FROM 
-  metal_bands 
-GROUP BY 
-  origin;
-SELECT 
-  origin, 
-  nb_fans 
-FROM 
-  temp_country_fans 
-ORDER BY 
-  nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
