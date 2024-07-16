715a4008ee409352e13dc119acca3f307a350bd0
Evans Manyala <evans.manyala@gmail.com>
2024-07-16 14:12:20 +0300

    Fixing errors

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,7 +10,7 @@ from pymongo import MongoClient
 def update_topics(mongo_collection, name, topics):
     """update school topics based on the name"""
     result = mongo_collection.update_one(
-        {"name": name},  # Filter to find the document by name
-        {"$set": {"topics": topics}}  # Update the "topics" field
+        {"name": name},
+        {"$set": {"topics": topics}}
     )
     return result
