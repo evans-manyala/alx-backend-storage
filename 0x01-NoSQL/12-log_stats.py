521d3cbe05e909eadc1a04e549b8a4a68b3515e8
Evans Manyala <evans.manyala@gmail.com>
2024-07-16 23:50:42 +0300

    Error Fix 2

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,22 +6,14 @@ analyze Nginx logs in a MongoDB database.
 from pymongo import MongoClient
 
 
-def get_stats():
-    """"
-    Provides statistics about Nginx logs
-    in a MongoDB collection.
-    """
-    client = MongoClient('mongodb://localhost:27017')
-    nginx = client.logs.nginx
-    print(f"{nginx.count_documents({})} logs")
-    print("Methods:")
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        count = nginx.count_documents({'method': method})
-        print(f"\tmethod {method}: {count}")
-    get_count = nginx.count_documents({'method': 'GET', 'path': '/status'})
-    print(f"{get_count} status check")
-
-
 if __name__ == "__main__":
-    get_stats()
+    '''provides some stats about Nginx logs'''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    col = client.logs.nginx
+    print("{} logs".format(col.estimated_document_count()))
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = col.count_documents({'method': method})
+        print("\tmethod {}: {}".format(method, count))
+    status_get = col.count_documents({'method': 'GET', 'path': "/status"})
+    print("{} status check".format(status_get))
