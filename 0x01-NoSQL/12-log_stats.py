d958e815c6bee131c2bda6d918eb9846a5ad74d1
Evans Manyala <evans.manyala@gmail.com>
2024-07-16 16:57:31 +0300

    Adding script

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""This module provides functions to
+analyze Nginx logs in a MongoDB database.
+"""
+
+from pymongo import MongoClient
+
+def nginx_stats(mongo_collection):
+    """
+    Provides statistics about Nginx logs
+    in a MongoDB collection.
+    """
+    
+
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod={method}: {count}")  
+
+
+    status_logs = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"\tmethod=GET path=/status: {status_logs}")
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    nginx_collection = db['nginx']
+
+    nginx_stats(nginx_collection)
