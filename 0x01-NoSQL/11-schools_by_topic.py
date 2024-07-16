ed67a85b14804bab1627536de0de4494fb7beecd
Evans Manyala <evans.manyala@gmail.com>
2024-07-16 15:28:45 +0300

    Adding error fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-
 """
 This module provides functions
 for querying MongoDB collections.
 """
-
-
-from pymongo import MongoClient
 
 
 def schools_by_topic(mongo_collection, topic):
@@ -14,5 +10,5 @@ def schools_by_topic(mongo_collection, topic):
     Retrieves a list of schools offering a specific topic.
     """
 
-    query = {"topics": topic}
-    schools = list(mongo_collection.find(query))
+    schools = list(mongo_collection.find({"topics": topic}))
+    return schools
