c8ab144dba703e4bb2df4ab9c6675d045d694db2
Evans Manyala <evans.manyala@gmail.com>
2024-07-16 13:49:20 +0300

    Adding more code logic

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,3 +7,10 @@ for updating documents in MongoDB collections
 from pymongo import MongoClient
 
 
+def update_topics(mongo_collection, name, topics):
+    """update school topics based on the name"""
+    result = mongo_collection.update_one(
+        {"name": name},  # Filter to find the document by name
+        {"$set": {"topics": topics}}  # Update the "topics" field
+    )
+    return result
