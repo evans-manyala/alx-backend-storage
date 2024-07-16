8a92c0f6fb7a405f1c20ec4a98d5b71c406418d8
Evans Manyala <evans.manyala@gmail.com>
2024-07-16 13:10:59 +0300

    Task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Funtions lists all documents in a collection
+"""
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """Function to list all documents"""
+    documents = mongo_collection.find()
+    return documents
