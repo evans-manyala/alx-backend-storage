c186cd29fb937ed3c47103e5ab3b910aade6d1ba
Evans Manyala <evans.manyala@gmail.com>
2024-07-17 08:19:05 +0300

    Errof Fix 4

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,7 +2,6 @@
 """This module provides functions to
 analyze Nginx logs in a MongoDB database.
 """
-
 from pymongo import MongoClient
 
 
@@ -11,22 +10,34 @@ def nginx_stats(mongo_collection):
     Provides statistics about Nginx logs
     in a MongoDB collection.
     """
-    total_logs = mongo_collection.count_documents()
+
+    total_logs = mongo_collection.count_documents({})
     print(f"{total_logs} logs")
 
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    if total_logs == 0:
+        print("Methods:")
+        for method in methods:
+            print(f"    method {method}: 0")
+        print(f"0 status check")
+        return
+
+    print("Methods:")
     for method in methods:
         count = mongo_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print(f"    method {method}: {count}")
 
-    status_logs = mongo_collection.count_documents
-    ({"method": "GET", "path": "/status"})
-    print(f"{status_logs} status check")
+    status_check_count = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check_count} status check")
 
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient(
+        "mongodb://localhost:27017/"
+    )
     db = client["logs"]
     nginx_collection = db["nginx"]
 
