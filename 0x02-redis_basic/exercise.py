267c54d9ed3d6c9b8f278617a26e086163fb17fc
Evans Manyala <evans.manyala@gmail.com>
2024-07-17 23:05:10 +0300

    Adding more code logic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -44,3 +44,66 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self,
+        key: str,
+        fn: Optional[Callable[[bytes], Union[str, int, float, bytes]]] = None,
+    ) -> Optional[Union[str, int, float, bytes]]:
+        """
+        Retrieve data from the Redis cache using the key and optionally
+        convert it using the provided callable.
+
+        Parameters
+        ----------
+        key : str
+            The key associated with the data to retrieve.
+        fn : Optional[Callable[[bytes], Union[str, int, float, bytes]]]
+            A callable to convert the data to the desired format.
+
+        Returns
+        -------
+        Optional[Union[str, int, float, bytes]]
+            The data retrieved from the cache, optionally converted.
+            Returns None if the key does not exist.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data from the Redis cache as a string.
+
+        Parameters
+        ----------
+        key : str
+            The key associated with the data to retrieve.
+
+        Returns
+        -------
+        Optional[str]
+            The data retrieved from the cache as a string.
+            Returns None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data from the Redis cache as an integer.
+
+        Parameters
+        ----------
+        key : str
+            The key associated with the data to retrieve.
+
+        Returns
+        -------
+        Optional[int]
+            The data retrieved from the cache as an integer.
+            Returns None if the key does not exist.
+        """
+        return self.get(key, fn=int)
